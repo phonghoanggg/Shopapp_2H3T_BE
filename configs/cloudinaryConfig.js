ee27b0ccf7c0306a1b0970d2974759c90887df13
phonghoang <phonghoang@gmail.com>
2025-05-07 12:07:54 +0900

    fix .env

--- configs/cloudinaryConfig.js
+++ configs/cloudinaryConfig.js
@@ -1,8 +1,14 @@
 import dotenv from "dotenv";
-import { v2 as cloudinary } from 'cloudinary';
+import pkg from "cloudinary";
+
+dotenv.config(); // Đảm bảo biến môi trường được tải
+
+const { v2: cloudinary } = pkg;
+
 cloudinary.config({ 
-  cloud_name: 'dgcpmlway', 
+  cloud_name: "dgcpmlway",
   api_key: process.env.API_KEY_CLOUDINARY, 
   api_secret: process.env.API_SECRIT_CLOUDINARY
 });
+
 export default cloudinary;
